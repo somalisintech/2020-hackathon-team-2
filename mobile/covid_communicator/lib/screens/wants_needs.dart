48a9cf1e09fbaeee842cb3ec3c9988c2c9adef08
MHUS25 <mmhussein125@gmail.com>
2020-10-25 12:40:52 +0000

    Adds wants and needs screen

--- mobile/covid_communicator/lib/screens/wants_needs.dart
+++ mobile/covid_communicator/lib/screens/wants_needs.dart
@@ -9,10 +9,47 @@ class _WantsNeedsScreenState extends State<WantsNeedsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
+      appBar: AppBar(
+        backgroundColor: kMainColor,
+        title: Text("Somali ICU Covid Communication"),
+      ),
+       body: Container(
         child: SafeArea(
-          child: Center(
-            child: Text("About Page"),
+          child: GridView.count(
+            // Create a grid with 2 columns. If you change the scrollDirection to
+            // horizontal, this produces 2 rows.
+            crossAxisCount: 2,
+            // Generate 100 widgets that display their index in the List.
+            children: List.generate(WantsNeeds.keys.length, (index) {
+              return Center(
+                child: ReusableCard(
+                  color: kMenuButtonActiveColor,
+                  cardChild: Expanded(
+                    child: Column(
+                      children: [
+                        Divider(
+                          height: 10.0,
+                        ),
+                        Image.asset(
+                          WantsNeeds.keys[index].image,
+                          width: 60.0,
+                        ),
+                        Divider(
+                          height: 10.0,
+                        ),
+                        Text(
+                          WantsNeeds.keys[index].description,
+                          style: kLabelTextStyle,
+                        )
+                      ],
+                    ),
+                  ),
+                  onPress: () {
+                    play_sound().Local(WantsNeeds.keys[index].audio);
+                  },
+                ),
+              );
+            }),
           ),
         ),
       ),
